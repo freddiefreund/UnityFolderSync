3e43f3ac63534faae530ffb92a45ec8f5e9f5ef8
Freddie <frederickfreund@gmail.com>
2021-10-16 12:48:26 +0200

    added gui input fields for dest and src path

--- Window.cs
+++ Window.cs
@@ -7,16 +7,22 @@ using UnityEngine;
 public class Window : EditorWindow
 {
     private string myString = "Hello Person!";
+    private string source = "enter src path here...";
+    private string destination = "enter dest path here...";
+
     [MenuItem("Window/example")]
     public static void ShowWindow()
     {
         GetWindow<Window>("Example");
     }
+
     private void OnGUI()
     {
         GUILayout.Label("This is a Label.", EditorStyles.boldLabel);
 
         myString = EditorGUILayout.TextField("Name", myString);
+        source = EditorGUILayout.TextField("sourceFilePath", source);
+        destination = EditorGUILayout.TextField("destinationFilePath", destination);
 
         if (GUILayout.Button("Press Me pls"))
         {
