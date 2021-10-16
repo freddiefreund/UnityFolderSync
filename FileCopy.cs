f98795a97e571b5ce397bf4878ad6a237bb36d5e
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-10-16 13:35:48 +0200

    add FileCopy.cs

---
+++ FileCopy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+public class FileCopy
+{
+    public static void CopyIfNewer(String srcDir, String destDir)
+    {
+        string[] originalFiles = Directory.GetFiles(srcDir, "*", SearchOption.AllDirectories);
+
+        Array.ForEach(originalFiles, (originalFileLocation) =>
+        {
+            FileInfo originalFile = new FileInfo(originalFileLocation);
+            FileInfo destFile = new FileInfo(originalFileLocation.Replace(srcDir, destDir));
+
+            if (destFile.Exists)
+            {
+                if (originalFile.Length > destFile.Length)
+                {
+                    originalFile.CopyTo(destFile.FullName, true);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(destFile.DirectoryName);
+                originalFile.CopyTo(destFile.FullName, false);
+            }
+        });
+    }
+}
