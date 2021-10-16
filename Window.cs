3b94baccc9232638e5c6ad241ce50dcd4054e496
Freddie <frederickfreund@gmail.com>
2021-10-16 13:34:52 +0200

    Added folder browsing and timer

--- Window.cs
+++ Window.cs
@@ -9,11 +9,14 @@ public class Window : EditorWindow
     private string myString = "Hello Person!";
     private string source = "enter src path here...";
     private string destination = "enter dest path here...";
-
+    private float timerInterval = 10f;
+    private float timer;
+    
     [MenuItem("Window/example")]
     public static void ShowWindow()
     {
         GetWindow<Window>("Example");
+        
     }
 
     private void OnGUI()
@@ -22,11 +25,27 @@ public class Window : EditorWindow
 
         myString = EditorGUILayout.TextField("Name", myString);
         source = EditorGUILayout.TextField("sourceFilePath", source);
+        if (GUILayout.Button("Browse"))
+        {
+            source = EditorUtility.OpenFolderPanel("Source folder", "", "");
+        }
         destination = EditorGUILayout.TextField("destinationFilePath", destination);
+        if (GUILayout.Button("Browse"))
+        {
+            destination = EditorUtility.OpenFolderPanel("Destination folder", "", "");
+        }
 
+        if (EditorApplication.timeSinceStartup > timer)
+        {
+            timer += timerInterval;
+            Debug.Log("10 Seconds passed");
+            // Call Bastis function
+        }
+        
         if (GUILayout.Button("Press Me pls"))
         {
             Debug.Log("Button pressed!");
+            myString = $"{EditorApplication.timeSinceStartup.ToString("N1")} Timer:{timer.ToString("N1")}";
         }
     }
 }
