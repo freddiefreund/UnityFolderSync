b4e80026f92d60132f7ac1dc979752236a451ce5
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-10-16 14:04:13 +0200

    add FileInfos.cs

---
+++ FileInfos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class FileInfos
+{
+    public static List<String> GetNewFiles(String srcDir, String destDir)
+    {
+        List<String> newFiles = new List<string>();
+        string[] srcFiles = Directory.GetFiles(srcDir, "*", SearchOption.AllDirectories);
+
+        Array.ForEach(srcFiles, (srcFileLocation) =>
+        {
+            FileInfo srcFile = new FileInfo(srcFileLocation);
+            FileInfo destFile = new FileInfo(srcFileLocation.Replace(srcDir, destDir));
+
+            if (destFile.Exists)
+            {
+                if (srcFile.LastWriteTime > destFile.LastWriteTime)
+                {
+                    srcFile.CopyTo(destFile.FullName, true);
+                    newFiles.Add(destFile.FullName);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(destFile.DirectoryName);
+                newFiles.Add(destFile.FullName);
+            }
+        });
+        return newFiles;
+    }
+}
